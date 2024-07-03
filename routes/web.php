3bfd4924e13a2ebd84086b7c83b2074fb0ab5ad4
Welmann Paniagua <welmann2903@gmail.com>
2024-07-02 22:42:50 -0600

    Create new posts done

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,22 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+// Principal Routes
+Route::get('/', [HomeController::class, 'homePage'])->name('login');
 
-Route::get('/', [HomeController::class, 'homePage']);
+// User routes
+Route::post('/register', [UserController::class, 'register'])->middleware('guest');
+Route::post('/login', [UserController::class, 'login'])->middleware('guest');
+Route::post('/logout', [UserController::class, 'logout'])->middleware('loggedInCheck');
 
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login']);
-Route::post('/logout', [UserController::class, 'logout']);
+// Blog routes
+Route::get('/create-post', [BlogController::class, 'showCreatePage'])->middleware('loggedInCheck');
+Route::post('/create-post', [BlogController::class, 'createNewPost'])->middleware('loggedInCheck');
+Route::get('/post/{post}', [BlogController::class, 'viewPost'])->middleware('loggedInCheck');
+
+// Profile routes
+Route::get('/profile/{user}', []);
